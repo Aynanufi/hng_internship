e3a1b72bcc97072610f354a84ba16e912a97b79f
aynanufi <nanyaokolie@gmail.com>
2018-04-14 23:12:33 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>HNG Task 1</title>
+  <link rel="stylesheet" href="css/style.css" type="text/css"/>
+  <link href="https://fonts.googleapis.com/css?family=Rum+Raisin" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Risque" rel="stylesheet">
+</head>
+<body>
+<ul>
+  <li> <a href="#">HomeBoyz Cafe-Rhum Whisky</a></li>
+  <li> <a href="#">WHISKY</a></li>
+  <li> <a href="#">OTHER SPIRITS</a></li>
+  <li> <a href="#">THE DISTILLERY</a></li>
+
+</ul>
+
+<img src="img/cafe.jpg" alt="HomeBoyz cafe-rhum whisky image" />
+
+<div class="bottom">
+  <p>Feel at home with the world's best café rhum whisky</p>
+
+<p style="color: red; ">
+<?php
+date_default_timezone_set("Africa/Lagos");
+echo date("H:i:s");
+?>
+</p>
+</div>
+
+
+    
+</body>
+</html> 
